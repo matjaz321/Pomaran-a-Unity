8745866672dc694129913840709a260b677e4f59
matjaz321 <matjaz.zavski15@gmail.com>
2016-05-12 22:23:32 +0200

    animacija

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,11 +5,13 @@ public class CharacterMovement : MonoBehaviour {
 
 	[SerializeField] private float BoostSpeed;
 	[SerializeField] private float speed;
-	[SerializeField] private float Jump;
+	[SerializeField] private float JumpSpeed = 1500;
 	[SerializeField] private Transform GroundDetection;
 	[SerializeField] private LayerMask Ground;
 	[SerializeField] private int NumberOfBoost;
+	[SerializeField] private GameObject TransitionParticle;
 
+	private Animator myAnim;
 	private Rigidbody2D myRb;
 	private bool grounded;
 	private bool Boost = false;
@@ -17,6 +19,7 @@ public class CharacterMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		myRb = gameObject.GetComponent<Rigidbody2D> ();
+		myAnim = gameObject.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -25,7 +28,13 @@ public class CharacterMovement : MonoBehaviour {
 		grounded = Physics2D.OverlapCircle (GroundDetection.position, 0.1f, Ground);
 
 		if (Input.GetKey (KeyCode.UpArrow) && grounded) {
-			myRb.velocity = new Vector2 (myRb.velocity.x, Jump * Time.deltaTime);
+			if (myRb.velocity.x > 0) {
+				myAnim.SetTrigger ("jump_right");
+			} else {
+				myAnim.SetTrigger ("jump_left");
+			}
+
+			myRb.velocity = new Vector2 (myRb.velocity.x, JumpSpeed * Time.deltaTime);
 		}
 
 		if (Input.GetKey (KeyCode.RightArrow)) {
@@ -40,9 +49,12 @@ public class CharacterMovement : MonoBehaviour {
 		}
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			if (NumberOfBoost > 0) {
+			if (NumberOfBoost > 0 && (myRb.velocity.x != 0 || myRb.velocity.y != 0)) {
 				NumberOfBoost -= 1;
 			
+				GameObject instantiatedParticle = Instantiate (TransitionParticle, transform.position, Quaternion.identity) as GameObject;
+				Destroy (instantiatedParticle, 3f);
+
 				if (myRb.velocity.x > 0) {
 					myRb.velocity = new Vector2 (myRb.velocity.x * BoostSpeed * Time.deltaTime, myRb.velocity.x * BoostSpeed * Time.deltaTime);
 				} else {
