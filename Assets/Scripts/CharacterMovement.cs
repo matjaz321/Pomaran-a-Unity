3f750cfccdf6b74982ace5b9990dba8cbace4642
matjaz321 <matjaz.zavski15@gmail.com>
2016-05-12 20:22:44 +0200

    boost

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	[SerializeField] private float BoostSpeed;
+	[SerializeField] private float speed;
+	[SerializeField] private float Jump;
+
+
+	private Rigidbody2D myRb;
+	private bool grounded;
+	private bool Boost = false;
+
+	// Use this for initialization
+	void Start () {
+		myRb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.Space)) {
+			myRb.velocity = new Vector2 (myRb.velocity.x, Jump * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			if(myRb.velocity.x <= (speed * Time.deltaTime))
+				myRb.velocity = new Vector2 (speed * Time.deltaTime, myRb.velocity.y);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			if(myRb.velocity.x >= (-speed * Time.deltaTime))
+				myRb.velocity = new Vector2 (-speed * Time.deltaTime, myRb.velocity.y);
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			myRb.velocity = new Vector2 (myRb.velocity.x * BoostSpeed * Time.deltaTime, myRb.velocity.y * BoostSpeed * Time.deltaTime);
+			//myRb.AddForce((Vector2.right + Vector2.up) * BoostSpeed * Time.deltaTime);
+		}
+	}
+		
+}
