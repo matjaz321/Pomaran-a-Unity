7fcf6dd3dd86f5ca55e513e2613c9a93bcea870c
matjaz321 <matjaz.zavski15@gmail.com>
2016-05-13 04:36:00 +0200

    visoka energija, pazi spječe

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField] private Transform Target;
+	[SerializeField] private float Smoothing;
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - Target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetCamPos = Target.position + offset;
+
+		transform.position = /*Vector3.Lerp (*/targetCamPos/*)*/;
+	}
+}
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -11,24 +11,48 @@ public class CharacterMovement : MonoBehaviour {
 	[SerializeField] private int NumberOfBoost;
 	[SerializeField] private ParticleSystem[] PlayerParticle;	
 	[SerializeField] private Material[] playerColors;
+	[SerializeField] private GameObject BoostSound;
+	[SerializeField] private GameObject hitGroundSound;
+	[SerializeField] private GameObject DropSound;
+	[SerializeField] private GameObject pickUpSound;
+	//[SerializeField] private Camera camera;
+	//[SerializeField] private GameObject Junk;
 
+
+	/*[SerializeField] private float duration = .5f;
+	private float elapsed = 0f;
+	private bool zoomIn = false;
+	private bool zoomOut = false;
+*/
+	//private
 	private MeshRenderer playerRenderer;
 	private Animator myAnim;
 	private Rigidbody2D myRb;
 	private bool grounded;
 	private bool Boost = false;
-
+	private bool jumped = false;
 	// Use this for initialization
 	void Start () {
 		myRb = gameObject.GetComponent<Rigidbody2D> ();
 		myAnim = gameObject.GetComponent<Animator> ();
 		playerRenderer = gameObject.GetComponent<MeshRenderer> ();
+		//camera = GetComponent<Camera> ();
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
+		 
+		/*if (zoomIn)
+			CameraZoomIn();
+		if (zoomOut)
+			CameraZoomOut();
+		*/
 
 		grounded = Physics2D.OverlapCircle (GroundDetection.position, 0.1f, Ground);
+		if (jumped && grounded){
+			hitGroundSound.GetComponent<AudioSource> ().Play ();
+			jumped = false;
+		}
 
 		if (Input.GetKey (KeyCode.UpArrow) && grounded) {
 			if (myRb.velocity.x > 0) {
@@ -38,7 +62,10 @@ public class CharacterMovement : MonoBehaviour {
 			}
 
 			myRb.velocity = new Vector2 (myRb.velocity.x, JumpSpeed * Time.deltaTime);
+			jumped = true;
 		}
+
+
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			if (myRb.velocity.x <= (speed * Time.deltaTime)) {
 				myRb.velocity = new Vector2 (speed * Time.deltaTime, myRb.velocity.y);
@@ -52,43 +79,93 @@ public class CharacterMovement : MonoBehaviour {
 		}
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			if (NumberOfBoost > 0 && (myRb.velocity.x != 0 || myRb.velocity.y != 0)) {
+			if (NumberOfBoost > 0) {
 				NumberOfBoost -= 1;
+				BoostSound.GetComponent<AudioSource> ().Play ();
+				//zoomOut = true;
 
 				if (NumberOfBoost == 2) {
-					playerRenderer.material = playerColors [2];
-					GameObject instantiatedParticle = Instantiate (PlayerParticle [1], transform.position, Quaternion.identity) as GameObject;
-					Destroy (instantiatedParticle, 3f);
-				} else if (NumberOfBoost == 1) {
+					gameObject.GetComponent<Transform> ().localScale = new Vector2 (1f, 1f);
 					playerRenderer.material = playerColors [1];
 					GameObject instantiatedParticle = Instantiate (PlayerParticle [2], transform.position, Quaternion.identity) as GameObject;
 					Destroy (instantiatedParticle, 3f);
-				} else if (NumberOfBoost == 0) {
+				} else if (NumberOfBoost == 1) {
+					gameObject.GetComponent<Transform> ().localScale = new Vector2 (.75f, .75f);
 					playerRenderer.material = playerColors [0];
-					GameObject instantiatedParticle = Instantiate (PlayerParticle[0], transform.position, Quaternion.identity) as GameObject;
+					GameObject instantiatedParticle = Instantiate (PlayerParticle [0], transform.position, Quaternion.identity) as GameObject;
+					Destroy (instantiatedParticle, 3f);
+				} else if (NumberOfBoost == 0) {
+					gameObject.GetComponent<Transform> ().localScale = new Vector2 (.5f, .5f);
+					playerRenderer.material = playerColors [2];
+					GameObject instantiatedParticle = Instantiate (PlayerParticle[1], transform.position, Quaternion.identity) as GameObject;
 					Destroy (instantiatedParticle, 3f);
 				}
-
-				//GameObject instantiatedParticle = Instantiate (TransitionParticle, transform.position, Quaternion.identity) as GameObject;
-				//Destroy (instantiatedParticle, 3f);
+					
 
 				if (myRb.velocity.x > 0) {
+					
 					myRb.velocity = new Vector2 (myRb.velocity.x * BoostSpeed * Time.deltaTime, myRb.velocity.x * BoostSpeed * Time.deltaTime);
 
-				} else {
+				} else if (myRb.velocity.x < 0) {
 					myRb.velocity = new Vector2 (myRb.velocity.x * BoostSpeed * Time.deltaTime, -myRb.velocity.x * BoostSpeed * Time.deltaTime);
-				} 
-
-				if (myRb.velocity.x == 0) {
-					myRb.velocity = new Vector2 (0, 2000  * Time.deltaTime);
+				} else {
+					myRb.velocity = new Vector2 (7 * BoostSpeed * Time.deltaTime, 0);
 				}
 
 				if (grounded) {
 					myRb.velocity = new Vector2 (myRb.velocity.x * BoostSpeed / 2 * Time.deltaTime, 0);
 				}
 					
-					
 			}
+		}
+	}
+
+	/*private void CameraZoomIn()
+	{
+		if (zoomIn) {
+			elapsed += Time.deltaTime / duration;
+			Camera.main.orthographicSize = Mathf.Lerp (8f, 5f, 0.8f);
+			if (Camera.main.orthographicSize == 5f) {
+				zoomIn = false;
+			}
+		}
+	}
+
+	private void CameraZoomOut()
+	{
+		if (zoomOut) {
+			elapsed += Time.deltaTime / duration;
+			Camera.main.orthographicSize = Mathf.Lerp (5f, 8f, 0.8f);
+			if (Camera.main.orthographicSize == 8f) {
+				zoomIn = true;
+				zoomOut = false;
+			}
+		}
+	}*/
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "food") {
+			Destroy (other.gameObject);
+			if (NumberOfBoost >= 2) {
+				DropSound.GetComponent<AudioSource> ().Play ();
+				GameObject instantiatedParticle = Instantiate (PlayerParticle [3], transform.position, Quaternion.identity) as GameObject;
+				NumberOfBoost = 2;
+			}else {
+				pickUpSound.GetComponent<AudioSource> ().Play ();
+				NumberOfBoost += 1;
+				if (NumberOfBoost == 1) {
+					gameObject.GetComponent<Transform> ().localScale = new Vector2 (.75f, .75f);
+					playerRenderer.material = playerColors [0];
+					GameObject instantiatedParticle = Instantiate (PlayerParticle[1], transform.position, Quaternion.identity) as GameObject;
+					Destroy (instantiatedParticle, 3f);
+				} else if (NumberOfBoost == 2) {
+					gameObject.GetComponent<Transform> ().localScale = new Vector2 (1f, 1f);
+					playerRenderer.material = playerColors [1];
+					GameObject instantiatedParticle = Instantiate (PlayerParticle [0], transform.position, Quaternion.identity) as GameObject;
+					Destroy (instantiatedParticle, 3f);
+				}
+			}
+			//Destroy (other.gameObject, 3f);
 		}
 	}
 }
